9e94ba70397b266c083c0cb6e3a027290488828f
domindominik <dominwro@gmail.com>
2024-07-13 13:15:50 +0200

    fixed error code 403

--- src/main/java/aitt/trainingapp_backend/service/UserService.java
+++ src/main/java/aitt/trainingapp_backend/service/UserService.java
@@ -1,11 +1,12 @@
 package aitt.trainingapp_backend.service;
 
 import aitt.trainingapp_backend.model.UserModel;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserModel saveUser(UserModel user);
-    UserModel findUserById(Long id);
-    String generateToken(UserModel user);
-    boolean checkPassword(String rawPassword, String encodedPassword);
-    UserModel findUserByEmail(String email);
+    //UserModel findUserById(Long id);
+    //boolean checkPassword(String rawPassword, String encodedPassword);
+    //UserModel findUserByEmail(String email);
+    String loginUser(UserModel user);
 }
--- src/main/java/aitt/trainingapp_backend/service/impl/UserServiceImpl.java
+++ src/main/java/aitt/trainingapp_backend/service/impl/UserServiceImpl.java
@@ -1,53 +1,47 @@
 package aitt.trainingapp_backend.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import aitt.trainingapp_backend.model.UserModel;
 import aitt.trainingapp_backend.repository.UserRepository;
 import aitt.trainingapp_backend.service.UserService;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import lombok.RequiredArgsConstructor;
+import aitt.trainingapp_backend.util.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
-
-import java.util.Date;
-import java.util.Optional;
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    private final String jwtSecret = "5VXPzUgs-qBSCrLPGto_TQ8Rw1e2WGMSC7qBz5dKMKQ=";
-    private final long jwtExpirationInMinutes = 1440;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final AuthenticationManager authenticationManager;
 
     @Override
     public UserModel saveUser(UserModel user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
-
     @Override
-    public UserModel findUserById(Long id) {
-        return userRepository.findById(id).orElse(null);
+    public String loginUser(UserModel user) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
+        } catch (Exception e) {
+            throw new RuntimeException("Invalid credentials");
+        }
+        final UserDetails userDetails = loadUserByUsername(user.getEmail());
+        return jwtTokenUtil.generateToken(userDetails);
     }
-
     @Override
-    public String generateToken(UserModel user) {
-        return Jwts.builder()
-                .setSubject(user.getEmail())
-                .setIssuedAt(new Date())
-                .setExpiration(new Date((new Date()).getTime() + jwtExpirationInMinutes))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
-                .compact();
-    }
-
-    @Override
-    public boolean checkPassword(String rawPassword, String encodedPassword) {
-        return bCryptPasswordEncoder.matches(rawPassword, encodedPassword);
-    }
-
-    @Override
-    public UserModel findUserByEmail(String email) {
-        return userRepository.findByEmail(email).orElse(null);
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserModel user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new User(user.getEmail(), user.getPassword(), Collections.emptyList());
     }
 }
