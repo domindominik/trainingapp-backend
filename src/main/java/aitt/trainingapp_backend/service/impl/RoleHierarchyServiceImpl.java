e6b94801734d95326086c9d7728e73b75a8e2eb0
domindominik <dominwro@gmail.com>
2024-07-28 22:26:58 +0200

    added @primery annotation

--- src/main/java/aitt/trainingapp_backend/service/impl/RoleHierarchyServiceImpl.java
+++ src/main/java/aitt/trainingapp_backend/service/impl/RoleHierarchyServiceImpl.java
@@ -1,13 +1,20 @@
 package aitt.trainingapp_backend.service.impl;
 
 import aitt.trainingapp_backend.service.RoleHierarchyService;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 
 @Service
+@Primary
 public class RoleHierarchyServiceImpl extends RoleHierarchyImpl implements RoleHierarchyService {
+    private static final Logger logger = LoggerFactory.getLogger(RoleHierarchyServiceImpl.class);
     public RoleHierarchyServiceImpl() {
         String hierarchy = "ROLE_ADMIN > ROLE_COACH\nROLE_COACH > ROLE_USER";
+        logger.info("Setting role hierarchy: {}", hierarchy);
         this.setHierarchy(hierarchy);
+        logger.info("Role hierarchy set successfully");
     }
 }
