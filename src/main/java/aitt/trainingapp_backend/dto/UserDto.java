c5c01a074fb45afc3f154e738eab0f760f859857
domindominik <dominwro@gmail.com>
2024-07-28 11:30:44 +0200

    added annotations

--- src/main/java/aitt/trainingapp_backend/dto/UserDto.java
+++ src/main/java/aitt/trainingapp_backend/dto/UserDto.java
@@ -1,5 +1,8 @@
 package aitt.trainingapp_backend.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -9,6 +12,10 @@ import lombok.Setter;
 @Setter
 public class UserDto {
     private Long id;
+    @NotEmpty(message = "Name cannot be empty")
+    @Email(message = "Email should be valid")
     private String email;
+    @NotEmpty(message = "Email cannot be empty")
+    @Size(min = 4, message = "Password should have at least 8 characters")
     private String password;
 }
